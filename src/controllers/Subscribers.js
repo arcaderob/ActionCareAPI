b3c63a79ea94ab2e84d11341dea48a95e5c94ea1
arcaderob <gill@arcaderob.com>
2022-11-26 19:25:48 -0400

    Add ability to insert subscriber

--- src/controllers/Subscribers.js
+++ src/controllers/Subscribers.js
@@ -1,6 +1,27 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password',
+    database : 'actioncare'
+});
+
 const createSubscriber = ({data}) => {
-    console.log('this is the data in create subscriber');
-    console.log(data);
+    return new Promise((resolve) => {
+        connection.query(`SELECT * FROM subscriptions WHERE patientEmail='${data.patient}' AND subscriberEmail='${data.subscriber}'`,
+        (error, results) => {
+            if (error) throw error;
+            if (results.length) return;
+
+            connection.query(`INSERT INTO subscriptions (patientEmail, subscriberEmail) VALUES ('${data.patient}', '${data.subscriber}')`,
+            (innerError, innerResults) => {
+                if (innerError) throw innerError;
+
+                resolve(innerResults[0])
+            });
+        });
+    });
 };
 
 module.exports = {
