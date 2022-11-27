ff6d5ce49cd702a743021b9f0ce771bae0dc0295
arcaderob <gill@arcaderob.com>
2022-11-27 17:30:48 -0400

    Send group notifications

--- src/controllers/CronJob.js
+++ src/controllers/CronJob.js
@@ -1,6 +1,7 @@
 const cron = require('node-cron');
 const mysql = require('mysql');
 const dayjs = require('dayjs');
+const axios = require('axios');
 
 const connection = mysql.createConnection({
     host     : 'localhost',
@@ -9,13 +10,53 @@ const connection = mysql.createConnection({
     database : 'actioncare'
 });
 
+const _formatDate = (date = new Date()) => dayjs(date).format('YYYY-MM-DDTHH:mm:ss.000[Z]');
+const _getTaskTime = (date) => dayjs(date).format('HH:mm');
+
+const _sendPushNotifications = (emails, task, time) => {
+    return new Promise((resolve) => {
+        axios.post(`https://app.nativenotify.com/api/indie/group/notification`, {
+            subIDs: emails,
+            appId: 5033,
+            appToken: 'iCsXYfWlLbPcCl9Dr0P1H9',
+            title: 'Missed Task',
+            message: `Missed ${task} at ${time}`,
+            })
+            .then((resp) => {
+                console.log('Notification Sent', resp);
+            })
+            .catch((e) => {
+                console.log('There is an axios error');
+            })
+            .finally(() => resolve());
+    });
+};
+
 const startCronJob = () => {
     cron.schedule('*/1 * * * *', () => {
-        const now = dayjs().format('YYYY-MM-DDTHH:mm:ss.000[Z]');
-        connection.query(`SELECT * FROM tasks WHERE daily=true AND DATE(completed) < (DATE('${now}') - INTERVAL 1 DAY) OR (daily=true AND completed IS NULL) OR (daily=false AND (DATE(datetime) < DATE('${now}')));`, (error, results) => {
+        const now = _formatDate();
+        connection.query(`SELECT * FROM tasks WHERE notification_sent=false AND daily=true AND DATE(completed) < (DATE('${now}') - INTERVAL 1 DAY) OR (notification_sent=false AND daily=true AND completed IS NULL) OR (notification_sent=false AND daily=false AND (DATE(datetime) < DATE('${now}')));`,
+        (error, results) => {
             if (error) throw error;
 
-            console.log(results);
+            results.forEach((row) => {
+                if (!row.notification_sent[0]) {
+                    connection.query(`SELECT * FROM subscriptions WHERE patientEmail='${row.email}'`,
+                        async (innerError, innerResults) => {
+                            if (innerError) throw innerError;
+
+                            if (innerResults.length) {
+                                const emails = [row.email];
+                                innerResults.forEach((item) => {
+                                    emails.push(item.subscriberEmail);
+                                });
+
+                                await _sendPushNotifications(emails, row.task, _getTaskTime(row.datetime));
+                            }
+                        }
+                    );
+                }
+            });
         });
     });
 };
